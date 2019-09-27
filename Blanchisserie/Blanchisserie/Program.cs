e2828cdb3abc81415f33b2e9adabb8d16f39a940
Jonathan Bertholom <j-bertholom@live.fr>
2019-09-27 11:57:09 +0200

    commit1

---
+++ Blanchisserie/Blanchisserie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blanchisserie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
